0671d626e5efa9557e491c67a4ce321a554f323b
pratham0301 <70634170+pratham0301@users.noreply.github.com>
2025-11-04 20:40:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+* {
+  padding: 0;
+  border: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color: rgb(161, 100, 223);
+}
+#buttons {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  min-height: 100vh;
+}
+
+.btn {
+  background-color: rgb(102, 51, 153);
+  color: white;
+  padding: 10px 20px;
+	cursor:pointer;
+}
